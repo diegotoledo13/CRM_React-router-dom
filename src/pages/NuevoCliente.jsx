dc044670a2991a22b20883bd59fbafdec3264169
Diego Toledo <diegomasteruno13@gmail.com>
2023-11-23 14:23:59 -0300

    231123

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Form } from 'react-router-dom'
+import Formulario from '../components/Formulario'
+
+export async function action({request}){
+  const formData = await request.formData()
+  const datos = Object.fromEntries(formData)
+  console.log(datos)
+  return {}
+}
 
 const NuevoCliente = () => {
   const navigate = useNavigate()
@@ -11,6 +19,15 @@ const NuevoCliente = () => {
         <button type="button"
                 className=' bg-blue-800 text-white px-3 py-1 font-bold uppercase'
                 onClick={() => navigate(-1)}>Volver</button>
+      </div>
+      <div className=' bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        <Form method='post'>
+          <Formulario/>
+          <input type="submit"
+                 value="Registrar Cliente" 
+                 className=' mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
+          />
+         </Form>
       </div>
     </>
   )
