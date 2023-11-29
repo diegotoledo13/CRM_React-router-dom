fefbdf37075c108dc7a8e0e485e7c835f0ffd26e
Diego Toledo <diegomasteruno13@gmail.com>
2023-11-29 12:28:29 -0300

    terminando app crm

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-
-const Error = ({children}) => {
+function Error({children}) {
   return (
-    <div className=' text-center my-4 bg-red-600 text-white font-bold p-3 uppercase'>
+    <div className="text-center my-4 bg-red-600 text-white font-bold p-3 uppercase">
         {children}
     </div>
   )
--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,9 +1,20 @@
-import { useNavigate, Form, useActionData, redirect } from 'react-router-dom'
+import { Form, useNavigate, useLoaderData, useActionData, redirect } from 'react-router-dom'
+import { obtenerCliente, actualizarCliente } from '../data/clientes'
 import Formulario from '../components/Formulario'
 import Error from '../components/Error'
-import { agregarCliente } from '../data/clientes'
 
-export async function action({request}) {
+export async function loader({params}) {
+    const cliente = await obtenerCliente(params.clienteId)
+    if(Object.values(cliente).length === 0) {
+        throw new Response('', {
+            status: 404,
+            statusText: 'El Cliente no fue encontrado'
+        })
+    }
+    return cliente
+}
+
+export async function action({request, params}) {
     const formData = await request.formData()
     const datos = Object.fromEntries(formData)
     const email = formData.get('email')
@@ -23,19 +34,21 @@ export async function action({request}) {
     if(Object.keys(errores).length) {
         return errores
     }
-    await agregarCliente(datos)
+
+    // Actualizar el cliente
+    await actualizarCliente(params.clienteId, datos)
     return redirect('/')
 }
 
-function NuevoCliente() {
-
+function EditarCliente() {
+    const navigate = useNavigate();
+    const cliente = useLoaderData()
     const errores = useActionData()
-    const navigate = useNavigate()
 
     return (
         <>
-            <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
-            <p className="mt-3">Llena todos los campos para registrar un nuevo cliente</p>
+            <h1 className="font-black text-4xl text-blue-900">Editar Cliente</h1>
+            <p className="mt-3">A continuación podrás modificar los datos de un cliente</p>
 
             <div className="flex justify-end">
                 <button
@@ -55,12 +68,14 @@ function NuevoCliente() {
                     method='post'
                     noValidate
                 >
-                    <Formulario />
+                    <Formulario 
+                        cliente={cliente}
+                    />
 
                     <input 
                         type="submit"
                         className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
-                        value="Registrar Cliente"
+                        value="Guardar Cambios"
                     />
                 </Form>
             </div>
@@ -68,4 +83,4 @@ function NuevoCliente() {
     )
 }
 
-export default NuevoCliente
+export default EditarCliente
